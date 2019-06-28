a376e882b30c63fc38a6eaa66853e13f992e4a20
Xuffasch <tristan.lucas@gmail.com>
2019-06-28 16:42:10 +0200

    Added ChatMessage class

--- friendlychat/lib/main.dart
+++ friendlychat/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const String _name = "Tristan";
+
 void main() {
   runApp(new FriendlychatApp());
 }
@@ -68,4 +70,40 @@ class ChatScreenState extends State<ChatScreen> {
       body: _buildTextComposer(),
     );
   }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
+
+  @override 
+  Widget build(BuildContext context) {
+    return Container (
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      // Row to contain an avatar and a bloc of 2 vertically 
+      // stacked Text, horizontally distributed 
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          // Container to position the avatar
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text(_name[0])),
+          ),
+          // Container to position the texts
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(_name, style: Theme.of(context).textTheme.subhead),
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
 }
